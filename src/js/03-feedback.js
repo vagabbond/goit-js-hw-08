320651591e3f838b289357314c1ee73d477b7246
Maks Thimbarov <imaksim2@icloud.com>
2022-12-03 00:50:39 +0200

    redacted

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,32 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const formInput = document.querySelector('.feedback-form input');
+const formTextarea = document.querySelector('.feedback-form textarea');
 const formInfo = {};
+const localStorageId = 'feedback-form-state';
+
+onFormRedact();
+
 form.addEventListener('input', throttle(onInputChange, 500));
+form.addEventListener('submit', onButtonChange);
 function onInputChange(event) {
   formInfo[event.target.name] = event.target.value;
-  localStorage.setItem('feedback', JSON.stringify(formInfo));
+  localStorage.setItem(localStorageId, JSON.stringify(formInfo));
 }
-form.addEventListener('submit', onButtonChange);
 
 function onButtonChange(event) {
   event.preventDefault();
-  console.log(JSON.parse(localStorage.getItem('feedback')));
-  localStorage.removeItem('feedback');
+  console.log(JSON.parse(localStorage.getItem(localStorageId)));
+  localStorage.removeItem(localStorageId);
   event.currentTarget.reset();
+}
+
+function onFormRedact() {
+  const savedItems = localStorage.getItem(localStorageId);
+  const parsedItems = JSON.parse(localStorage.getItem(localStorageId));
+  if (savedItems) {
+    formInput.value = parsedItems.email;
+    formTextarea.value = parsedItems.message;
+  }
 }
