5c1838c2a087f026e24f0341cdbb67786b2b9323
Maks Thimbarov <imaksim2@icloud.com>
2022-12-04 00:13:23 +0200

    redacted

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,28 +5,42 @@ const formInput = document.querySelector('.feedback-form input');
 const formTextarea = document.querySelector('.feedback-form textarea');
 const formInfo = {};
 const localStorageId = 'feedback-form-state';
+let parsedItems = {
+  email: '',
+  message: '',
+};
 
 onFormRedact();
 
 form.addEventListener('input', throttle(onInputChange, 500));
-form.addEventListener('submit', onButtonChange);
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(localStorageId)));
+  localStorage.removeItem(localStorageId);
+  event.currentTarget.reset();
+});
 function onInputChange(event) {
   formInfo[event.target.name] = event.target.value;
   localStorage.setItem(localStorageId, JSON.stringify(formInfo));
 }
 
-function onButtonChange(event) {
-  event.preventDefault();
-  console.log(JSON.parse(localStorage.getItem(localStorageId)));
-  localStorage.removeItem(localStorageId);
-  event.currentTarget.reset();
-}
-
 function onFormRedact() {
-  const savedItems = localStorage.getItem(localStorageId);
-  const parsedItems = JSON.parse(localStorage.getItem(localStorageId));
-  if (savedItems) {
+  if (localStorage.getItem(localStorageId)) {
+    parsedItems = JSON.parse(localStorage.getItem(localStorageId));
+  } else {
+    parsedItems = {
+      email: '',
+      message: '',
+    };
+  }
+  if (parsedItems.email !== undefined && parsedItems.email !== null) {
     formInput.value = parsedItems.email;
+  } else {
+    formInput.value = '';
+  }
+  if (parsedItems.message !== undefined && parsedItems.message !== null) {
     formTextarea.value = parsedItems.message;
+  } else {
+    formTextarea.value = '';
   }
 }
